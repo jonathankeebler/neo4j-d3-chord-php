1a500f49a0b37c3c3bd7b393716792581918f4b0
Jonathan Keebler <jonathan@scribblelive.com>
2014-07-22 16:47:14 -0400

    Hook up to neo4j on local

--- graph.php
+++ graph.php
@@ -11,40 +11,24 @@ class Graph {
 	protected $client = null;
 
 	public function __construct() {
-		$this->client = new Client();
+		$this->client = new Client("localhost", 7474);
 	}
 
 	public function create() {
-		$names = ['Mehdi', 'Kevin', 'Marie', 'Elo', 'Foof', 'Clement', 'Chris', 'Antoine', 'Julien', 'Nico', 'Yves', 'Pierro'];
-
-		$nodesIndex = new Index($this->client, Index::TypeNode, 'nodes_index');
-
-		$batch = new Batch($this->client);
-		foreach ($names as $name) {
-			$node = $this->client->makeNode()->setProperty('name', $name);
-			static::$nodes[] = $node;
-			$batch->save($node); //maybe implicit
-			$batch->addToIndex($nodesIndex, $node, 'type', 'User');
-		}
-
-		foreach (static::$nodes as $k => $node) {
-			// Create random `following` relationships
-			$users = range(0, count(static::$nodes) - 1);
-			unset($users[$k]);
-			$following_ids = array_rand($users, rand(1, 5));
-			foreach ((array) $following_ids as $following_id) {
-				$follows = $node->relateTo(static::$nodes[$following_id], 'follows');
-				$batch->save($follows);
-			}
-		}
-
-		$batch->commit();
+		
 	}
 
 	public function matrix() {
-		$queryString = " START a = node:nodes_index(type='User') " .
-					   " MATCH a-[:follows]->b " .
-					   " RETURN a.name, collect(b.name) ";
+		$queryString = " MATCH (a:Person)-[*1..3]-(b:Person) " . 
+			" WHERE a <> b " . 
+			" RETURN a.name, collect(DISTINCT b.name) " .
+			" LIMIT 100 ";
+	
+		$queryString = " MATCH (a:Person)-[:POSTED]->(:Tweet)-[:USED]->(:Word)<-[:USED]-(:Tweet)<-[:POSTED]-(b:Person) " . 
+			" WHERE a <> b " . 
+			" RETURN a.name, collect(DISTINCT b.name) " .
+			" LIMIT 100 ";
+			
 
 		$query = new Query($this->client, $queryString);
 		return $query->getResultSet();
